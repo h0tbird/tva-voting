51caad13e869a8806a62ad3c6c3527260dd72a3c
Marc Villacorta <marc.villacorta@gmail.com>
2016-06-29 12:27:21 +0200

    Update to DockerConf 2016 version

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import make_response
-from utils import connect_to_redis
+from flask import Flask, render_template, request, make_response, g
+from redis import Redis
 import os
 import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Dev")
-option_b = os.getenv('OPTION_B', "Ops")
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
-redis = connect_to_redis("redis-tva.marathon")
 app = Flask(__name__)
 
+def get_redis():
+    if not hasattr(g, 'redis'):
+        g.redis = Redis(host="redis-tva.marathon", db=0, socket_timeout=5)
+    return g.redis
 
 @app.route("/", methods=['POST','GET'])
 def hello():
@@ -25,6 +25,7 @@ def hello():
     vote = None
 
     if request.method == 'POST':
+        redis = get_redis()
         vote = request.form['vote']
         data = json.dumps({'voter_id': voter_id, 'vote': vote})
         redis.rpush('votes', data)
@@ -41,4 +42,4 @@ def hello():
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
