c6f506df31207f0b5cc042151f5c48aa9f502cf3
Marc Villacorta <marc.villacorta@gmail.com>
2016-12-13 23:42:46 +0100

    Disable marathon-lb temporarily

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="marathon-lb", port=10012, db=0, socket_timeout=5)
+        g.redis = Redis(host="redis-tva.marathon", port=6379, db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
