261cfb4ed5f0db9a0bbf2485d8e7e7e5011c0031
Marc Villacorta <marc.villacorta@gmail.com>
2017-05-26 16:35:39 +0200

    Vote unicorns vs dragons

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Dev")
-option_b = os.getenv('OPTION_B', "Ops")
+option_a = os.getenv('OPTION_A', "Unicorns")
+option_b = os.getenv('OPTION_B', "Dragons")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
